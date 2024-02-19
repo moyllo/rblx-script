21138f2c0948722882fa0acd9acf1ed0d690d2b6
Moyllo <143634317+moyllo@users.noreply.github.com>
2024-02-19 17:00:26 +0100

    reset

--- TpAll.lua
+++ TpAll.lua
@@ -6,44 +6,45 @@ main.ResetOnSpawn = false
 
 local Frame = Instance.new("Frame")
 Frame.Parent = main
-Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255) -- Couleur de fond blanche
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255) -- White background color
 Frame.BorderSizePixel = 0
-Frame.Position = UDim2.new(0.5, -75, 0.5, -25) -- Positionné au centre de l'écran
-Frame.Size = UDim2.new(0, 150, 0, 50) -- Taille de la boîte
+Frame.Position = UDim2.new(0.5, -75, 0.5, -25) -- Centered on screen
+Frame.Size = UDim2.new(0, 150, 0, 50) -- Box size
 
 local tpButton = Instance.new("TextButton")
 tpButton.Name = "tpButton"
 tpButton.Parent = Frame
-tpButton.BackgroundColor3 = Color3.fromRGB(0, 255, 0) -- Couleur verte pour le bouton
+tpButton.BackgroundColor3 = Color3.fromRGB(0, 255, 0) -- Green color for button
 tpButton.BorderSizePixel = 0
 tpButton.Position = UDim2.new(0, 0, 0, 0)
-tpButton.Size = UDim2.new(1, 0, 1, 0) -- Remplit la boîte
+tpButton.Size = UDim2.new(1, 0, 1, 0) -- Fills the box
 tpButton.Font = Enum.Font.SourceSans
-tpButton.Text = "TP All"
-tpButton.TextColor3 = Color3.fromRGB(255, 255, 255) -- Texte en blanc
+tpButton.Text = "TP"
+tpButton.TextColor3 = Color3.fromRGB(255, 255, 255) -- White text
 tpButton.TextSize = 20
 
-local speaker = game:GetService("Players").LocalPlayer
+local isTeleporting = false
 
 tpButton.MouseButton1Click:Connect(function()
-    local plr = game:GetService("Players").LocalPlayer
-    for i,v in pairs(game:GetService("Players"):GetChildren()) do
-        if v.ClassName == "Player" and v.Name ~= plr.Name then
-            if v.Character:FindFirstChild("HumanoidRootPart") then
-                v.Character.HumanoidRootPart.CFrame = CFrame.new(plr.Character.HumanoidRootPart.Position)
+    if not isTeleporting then
+        isTeleporting = true
+        while isTeleporting do
+            local players = game:GetService("Players"):GetPlayers()
+            for _, player in ipairs(players) do
+                if player ~= game.Players.LocalPlayer then
+                    game.Players.LocalPlayer.Character:MoveTo(player.Character.HumanoidRootPart.Position)
+                end
             end
+            wait(0.1)
         end
+    else
+        isTeleporting = false
     end
-
 end)
 
--- Affichage de la notification
+-- Notification display
 game:GetService("StarterGui"):SetCore("SendNotification", { 
-    Title = "[TEST] TPAll GUI";
+    Title = "[TEST] TP All GUI";
     Text = "BY MOYLLO";
-    Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
-Duration = 5;
-
-main.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-Frame.Active = true
-Frame.Draggable = true
+    Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"
+})
