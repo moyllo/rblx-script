2e18880044544bc5c79983bdbef1239fbd463bb0
Moyllo <143634317+moyllo@users.noreply.github.com>
2024-04-05 22:47:15 +0200

    Update 2.lua

--- 2.lua
+++ 2.lua
@@ -1,10 +1,19 @@
--- Vérifier si le jeu prend en charge la fonction sendMessage
-if sendMessage then
-    -- Message à envoyer dans le chat
-    local messageToSend = "Bonjour tout le monde !"
+--Get the StarterGui service
+local StarterGui = game:GetService("StarterGui")
 
-    -- Appeler la fonction sendMessage avec le message à envoyer
-    sendMessage(messageToSend)
-else
-    print("La fonction sendMessage n'est pas disponible dans ce jeu.")
-end
+--Use a repeat to make sure we get our system message
+--The repeat will only stop once the function successfully runs
+repeat
+	wait() --Wait to prevent crashing
+	local Success = pcall(function()
+		--Run SetCore method inside the pcall
+		--The first argument of SetCore is the method we wish to use
+		--In this case, the second argument is a dictionary of data for the chat message
+		StarterGui:SetCore("ChatMakeSystemMessage", {
+			Text = "This is a system message"; --The chat message
+			Color = Color3.fromRGB(0, 255, 255); --Chat message color, defaults to white
+			Font = Enum.Font.SourceSansBold; --Chat message font, defaults to SourceSansBold
+			TextSize = 18 --Text size, defaults to 18
+		})
+	end)
+until Success
